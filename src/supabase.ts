4b4c2c69f5124cce2fec475cb2754aa620325401
SalohKhb <salohsoh2006@gmail.com>
2026-05-15 17:05:59 +0100

    update004

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://nskmyfsnvewllzzfpipl.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable__1ws_lwR2n29Hqj78VY1Rg_gPlzrr4B';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://supabase.com/dashboard/project/nyengmsybdsgjalqejxz';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_1V3YwQmaIZrod7_AdfiiwQ_6MnErABe';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
